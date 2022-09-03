5f268b7425c5ea32abe6e287e178e8da1c91961a
Sébastien Le Roux <sebastien.leroux@ipcms.unistra.fr>
2022-09-03 13:50:22 +0200

    Update build_rpm.sh

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 VERSION="1.1.0"
-rm -rf atomes-$VERSION
-tar -zxf atomes-$VERSION.tar.gz atomes-$VERSION
+if [ -d atomes-$VERSION ]; then
+  rm -rf atomes-$VERSION
+fi
+tar -zxf atomes-$VERSION.tar.gz
 fedpkg --release f36 local
 
