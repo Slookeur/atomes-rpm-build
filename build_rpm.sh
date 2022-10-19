3d81abf927e005f7ddf1c1569849843f1ecfdaaf
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2022-10-19 16:17:03 +0200

    Atomes rpm build: mer. 19 oct. 2022 16:17:03 CEST

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# The following line requires to have a *.spec file in the active directory
+# Getting the latest *.spec file from the github repo:
+wget https://github.com/Slookeur/Atomes-rpm-build/raw/main/Fedora/atomes.spec
 
 # Local build
 fedpkg --release f36 local
@@ -9,7 +10,6 @@ fedpkg --release f36 local
 version=1.1.7
 wget https://github.com/Slookeur/Atomes-GNU/archive/refs/tags/v$version.tar.gz
 mv v$version.tar.gz $HOME/rpmbuild/SOURCES/
-wget https://github.com/Slookeur/Atomes-rpm-build/raw/main/Fedora/atomes.spec
 rpmname=`rpmbuild -bs atomes.spec|grep 'rpm'|awk '{printf $NF}'`
 koid=`klist|grep slook@FEDORAPROJECT.ORG`
 if [ -z "$koid" ]; then
