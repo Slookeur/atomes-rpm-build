e13197416d7722541cfc904032c80b6c4a569486
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2022-10-19 16:19:41 +0200

    Atomes rpm build: mer. 19 oct. 2022 16:19:41 CEST

--- build_rpm.sh
+++ build_rpm.sh
@@ -3,13 +3,15 @@
 # Getting the latest *.spec file from the github repo:
 wget https://github.com/Slookeur/Atomes-rpm-build/raw/main/Fedora/atomes.spec
 
-# Local build
-fedpkg --release f36 local
-
-# Koji build
+# Getting the latest GNU tarball for the github repo:
 version=1.1.7
 wget https://github.com/Slookeur/Atomes-GNU/archive/refs/tags/v$version.tar.gz
 mv v$version.tar.gz $HOME/rpmbuild/SOURCES/
+
+# Local build
+# fedpkg --release f36 local
+
+# Koji build
 rpmname=`rpmbuild -bs atomes.spec|grep 'rpm'|awk '{printf $NF}'`
 koid=`klist|grep slook@FEDORAPROJECT.ORG`
 if [ -z "$koid" ]; then
