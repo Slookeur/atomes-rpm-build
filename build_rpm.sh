d1feeb8ef0eec116a19f91e3d4b04620d0451ec5
Slookeur <sebastien.leroux@ipcms.unistra.fr>
2022-10-19 16:25:33 +0200

    Atomes rpm build: mer. 19 oct. 2022 16:25:33 CEST

--- build_rpm.sh
+++ build_rpm.sh
@@ -8,11 +8,21 @@ version=1.1.7
 wget https://github.com/Slookeur/Atomes-GNU/archive/refs/tags/v$version.tar.gz
 mv v$version.tar.gz $HOME/rpmbuild/SOURCES/
 
+# Building the source rpm:
+rpmname=`rpmbuild -bs atomes.spec|grep 'rpm'|awk '{printf $NF}'`
+
 # Local build
 # fedpkg --release f36 local
 
+# Fedora review
+# On the bug [https://bugzilla.redhat.com/show_bug.cgi?id=2130607]
+# fedora-review -b 2130607
+
+# localy
+# cp $rpmname .
+# fedora-review -n atomes
+
 # Koji build
-rpmname=`rpmbuild -bs atomes.spec|grep 'rpm'|awk '{printf $NF}'`
 koid=`klist|grep slook@FEDORAPROJECT.ORG`
 if [ -z "$koid" ]; then
   fkinit -u slook
